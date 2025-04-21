02179d31a1cb8d404355073fc444cb99d7ceca67
ALW-N <officialalwintomy@gmail.com>
2025-04-21 23:58:17 +0530

    Commit-6

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import tseslint from '@typescript-eslint/eslint-plugin'
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -19,10 +19,15 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      // Disable unused-vars rule to avoid errors for unused imports
+      'no-unused-vars': 'off', 
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
+      // React hooks related rules
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn', // Optional: Enforce correct hook dependencies
     },
-  },
+  }
 )
